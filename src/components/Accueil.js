68976fd29810c6911a574d5d5441d6b0252cdd44
Maxime Le Duc <maximeld19@gmail.com>
2019-04-18 15:24:21 +0200

    Jean neymar

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -9,8 +9,10 @@ export default class Accueil extends Component {
   render() {
 		return (
 		      <div id="Accueil">
-		      	<img id="logoAccueil"src={require("../img/Pokémon-Dex.png")}/>
-		      	<p>Bienvenue sur Pokémon-Dex !</p>
+		      	<div id="topAcc">
+			      	<img id="logoAccueil"src={require("../img/Pokémon-Dex.png")}/>
+			      	<p>Bienvenue sur Pokémon-Dex !</p>
+		      	</div>
 		      	<div id="Description">
 		      		<p>Ici tu peu faire des choses, plein de chose avec les pokemon.</p>
 		      	</div>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class TopBar extends Component {
 	constructor(props){
 		super(props)
-		this.state = {name :"DRESSEUR", inputVal:""};
+		this.state = {inputVal:""};
 		this.changeInput = this.changeInput.bind(this);
 	}
 	changeInput(event){
@@ -15,15 +15,18 @@ export default class TopBar extends Component {
 		return (
 		      <div className="Profile">
 			      <div className='TopBar'>
-			      		<div className='profile_image'>
-			      			<img id="profile_pic" src="https://www.pokepedia.fr/images/thumb/2/24/Tortank-RFVF.png/500px-Tortank-RFVF.png" />
+			      		<div className='profile_image' onClick={() =>this.props.changePage("Accueil")}>
+			      			<img id="profile_pic" src={require("../img/Pokémon-Dex.png")} />
 			      		</div>
 			      		
 				      	<div className="Inputbox">
 				      		<input  id="Search" type="text" placeholder="     Search..." value={this.state.inputVal} onChange={this.changeInput}></input>
 				     	</div>
-				     	<div className="box a">
-			      			<a>{this.state.name}</a>
+				     	<div className="box a" onClick={() =>this.props.changePage("All")}>
+			      			<a>All</a>
+			      		</div>
+			      		<div className="box a" onClick={() =>this.props.changePage("All")}>
+			      			<a>Advanced search</a>
 			      		</div>
 				      	<div className="box a" onClick={() =>this.props.changePage("All")}>
 				      		<a >CATEGORIES</a>
